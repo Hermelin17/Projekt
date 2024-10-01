be84a4fb79cd96d431482c2ba770846659892e9b
Hermelin17 <teo_swtor@hotmail.com>
2024-10-01 19:07:37 +0200

    Initial commit

---
+++ build/CMakeFiles/Globalen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Globalen.dir/main.cpp.o"
+  "CMakeFiles/Globalen.dir/main.cpp.o.d"
+  "Globalen"
+  "Globalen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Globalen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/teohedelin/Skola/TNCG15/Projekt
+# Build directory: /Users/teohedelin/Skola/TNCG15/Projekt/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/glm-build")
